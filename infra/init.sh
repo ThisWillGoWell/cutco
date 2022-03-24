6b0de264c4b69b1b6a260fd4b518264a3dc5bdf9
William Gowell <will@williams-mbp.localdomain>
2022-03-24 15:08:50 -0500

    infra init

--- infra/init.sh
+++
@@ -1,6 +0,0 @@
-
-xport CDK_NEW_BOOTSTRAP=1
-npx cdk bootstrap aws://571558830047/us-east-2 \
-    --cloudformation-execution-policies arn:aws:iam::aws:policy/AdministratorAccess \
-    aws://571558830047/us-east-2
-
